22c1ddedbd9a08a5781504e98aca68f68df941d8
trunghieu87 <trunghieu87@943d00a8-ebdc-11de-b599-4d5b25800cad>
2010-03-07 17:05:15 +0000

    Thêm controller và Info cho BankDebt, BankInterestRate và Pack

---
+++ trunk/02. SourceCode/CtyHongPhat_26_01_2010/CtyHongPhat/CtyHongPhatDatabase/Table/BankDebtInfo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CtyHongPhatDatabase.Table
+{
+    public class BankDebtInfo
+    {
+        private int _bankDebtId;
+        private string _bankName;
+        private System.DateTime _borrowDate;
+        private decimal _debtAmount;
+        private decimal _paidAmount;
+        private System.DateTime _maturityDate;
+        private decimal _interestRate;
+        private int _deleted;
+        private string _createdBy;
+        private System.DateTime _createdDate;
+        private string _modifiedBy;
+        private System.DateTime _modifiedDate;
+
+        #region Public Properties
+        public int BankDebtId
+        {
+            get { return _bankDebtId; }
+            set { _bankDebtId = value; }
+        }
+
+        public string BankName
+        {
+            get { return _bankName; }
+            set { _bankName = value; }
+        }
+
+        public System.DateTime BorrowDate
+        {
+            get { return _borrowDate; }
+            set { _borrowDate = value; }
+        }
+
+        public decimal DebtAmount
+        {
+            get { return _debtAmount; }
+            set { _debtAmount = value; }
+        }
+
+        public decimal PaidAmount
+        {
+            get { return _paidAmount; }
+            set { _paidAmount = value; }
+        }
+
+        public System.DateTime MaturityDate
+        {
+            get { return _maturityDate; }
+            set { _maturityDate = value; }
+        }
+
+        public decimal InterestRate
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+        }
+
+        public int Deleted
+        {
+            get { return _deleted; }
+            set { _deleted = value; }
+        }
+
+        public string CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value; }
+        }
+
+        public System.DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+
+        public string ModifiedBy
+        {
+            get { return _modifiedBy; }
+            set { _modifiedBy = value; }
+        }
+
+        public System.DateTime ModifiedDate
+        {
+            get { return _modifiedDate; }
+            set { _modifiedDate = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/02. SourceCode/CtyHongPhat_26_01_2010/CtyHongPhat/CtyHongPhatDatabase/Table/PackInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CtyHongPhatDatabase.Table
+{
+    public class PackInfo
+    {
+        private int _packId;
+        private string _packName;
+        private int _quantity;
+        private System.DateTime _importDate;
+        private decimal _totalAmount;
+        private string _createdBy;
+        private System.DateTime _createdDate;
+        private string _modifiedBy;
+        private System.DateTime _modifiedDate;
+        private int _deleted;
+
+        #region Public Properties
+        public int PackId
+        {
+            get { return _packId; }
+            set { _packId = value; }
+        }
+
+        public string PackName
+        {
+            get { return _packName; }
+            set { _packName = value; }
+        }
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public System.DateTime ImportDate
+        {
+            get { return _importDate; }
+            set { _importDate = value; }
+        }
+
+        public decimal TotalAmount
+        {
+            get { return _totalAmount; }
+            set { _totalAmount = value; }
+        }
+
+        public string CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value; }
+        }
+
+        public System.DateTime CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+
+        public string ModifiedBy
+        {
+            get { return _modifiedBy; }
+            set { _modifiedBy = value; }
+        }
+
+        public System.DateTime ModifiedDate
+        {
+            get { return _modifiedDate; }
+            set { _modifiedDate = value; }
+        }
+
+        public int Deleted
+        {
+            get { return _deleted; }
+            set { _deleted = value; }
+        }
+
+        #endregion
+    }
+}
